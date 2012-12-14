026a238309a815a1748b372048c0935db8972c13
Bieu Hien <bieuhien@plumvillage.org>
2012-12-14 17:41:20 +0100

    temporary flexslider working on homepage

--- functions.php
+++ functions.php
@@ -7,13 +7,25 @@ add_action( "wp_enqueue_scripts", 'saltandpepper_load_javascript_files' );
 
 function saltandpepper_load_javascript_files() {
     wp_register_script( 
-        'flexslider',
+        'jquery.flexslider',
         get_stylesheet_directory_uri() . '/js/jquery.flexslider.js',
         array('jquery'),
         '2.1',
         true
     );
 
-    wp_enqueue_script( 'flexslider' );
+    wp_register_script( 
+        'saltandpepper.frontpage',
+        get_stylesheet_directory_uri() . '/js/saltandpepper.frontpage.js',
+        array('jquery', 'jquery.flexslider'),
+        '',
+        true
+    );
+
+
+    wp_enqueue_script( 'jquery.flexslider' );
+    if ( is_front_page() ) {
+        wp_enqueue_script( 'saltandpepper.frontpage' );
+    }
 }
 
