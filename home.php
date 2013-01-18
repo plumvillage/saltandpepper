a6e3772f34670e405f318fe91bc24d8910047947
Bieu Hien <bieuhien@plumvillage.org>
2013-01-18 16:35:52 +0100

    changed home.php to support full page width content for wooSlider

--- home.php
+++ home.php
@@ -16,31 +16,28 @@ if ( !defined('ABSPATH')) exit;
  * @copyright      2003 - 2012 ThemeID
  * @license        license.txt
  * @version        Release: 1.0
- * @filesource     wp-content/themes/responsive/home.php
+ * @filesource     wp-content/themes/responsive-child-theme/home.php
  * @link           http://codex.wordpress.org/Template_Hierarchy
  * @since          available since Release 1.0
  */
 ?>
-<?php get_header(); ?> 
-       <div id="featured" class="grid col-940 ">
-            <div class="flexslider">
-                <ul class="slides">
-                    <li>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/breathe.png" />
-                    </li>
-                    <li>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/community.jpg" />
-                    </li>
-                    <li>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/mindfulness.jpg" />
-                    </li>
-                    <li>
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/planVisit.jpg" />
-                    </li>
-                </ul>
-            </div><!-- end of .flexslider -->
-       
+<?php get_header(); ?>
+
+        <div id="featured" class="grid col-940">
+            
+            <?php $options = get_option('responsive_theme_options');
+			// First let's check if content is in place
+			    if (!empty($options['home_content_area'])) {
+					echo do_shortcode($options['home_content_area']);
+			// If not let's show dummy content for demo purposes
+			      } else { 
+			        echo '<p>';
+				    echo __('Your title, subtitle and this very content is editable from Theme Option. Call to Action button and its destination link as well. Image on your right can be an image or even YouTube video if you like.','responsive');
+				    echo '</p>';
+				  }
+			?>
+            
         </div><!-- end of #featured -->
-               
+             
 <?php get_sidebar('home'); ?>
 <?php get_footer(); ?>
